628d34aaa1c697708dd5c3f2da43481c3df63547
Shuyi-bomi <52222095+Shuyi-bomi@users.noreply.github.com>
2020-10-16 15:49:10 -0700

    Update plot.py

--- plot.py
+++ plot.py
@@ -5,3 +5,23 @@ Created on Fri Apr  3 15:02:26 2020
 project apm598
 @author: Shuyi Li
 """
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+from PIL import Image
+#from matplotlib import pyplot
+import matplotlib.pyplot as plt
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+import copy
+import dlib
+import cv2
+import os
+from imutils import face_utils
