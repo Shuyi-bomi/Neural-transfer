9b4ea59234524aa538e42a667a85ad6339b6023b
Shuyi-bomi <52222095+Shuyi-bomi@users.noreply.github.com>
2020-10-16 16:00:57 -0700

    Update plot.py

--- plot.py
+++ plot.py
@@ -14,14 +14,39 @@ import torch.optim as optim
 import numpy as np
 
 from PIL import Image
-#from matplotlib import pyplot
 import matplotlib.pyplot as plt
-
-import torchvision.transforms as transforms
-import torchvision.models as models
-
-import copy
-import dlib
-import cv2
 import os
-from imutils import face_utils
+
+#plot
+def preprocess(tensor):
+    image = tensor.cpu().clone()
+    image = image.squeeze(0) 
+    image = unloader(image)
+    return image
+outputst = preprocess(outputst)
+outputct = preprocess(outputct)
+fig, axes = plt.subplots(nrows=1, ncols=2)
+fig.set_size_inches(4, 4)
+axes[0].imshow(outputst)
+axes[0].set_title("Style Transfer")
+axes[1].imshow(outputct)
+axes[1].set_title("Content Transfer")
+
+plt.ioff()
+plt.show()
+
+
+# plot loss
+fig, axes = plt.subplots(nrows=1, ncols=2)
+fig.set_size_inches(8, 4)
+axes[0].plot(np.arange(320),losspro[:320,0],'--',label='Content Loss')
+axes[0].plot(np.arange(320),losspro[:320,1],'-.',label='Style Loss')
+axes[0].plot(np.arange(320),losspro[:320,2],':',label='Total Loss')
+axes[0].set_title("Style Transfer")
+axes[0].legend()
+axes[1].plot(np.arange(320),losspro[:320,3],'--',label='Content1 Loss(shepherd)')
+axes[1].plot(np.arange(320),losspro[:320,4],'-.',label='Content2 Loss(pomeranian)')
+axes[1].plot(np.arange(320),losspro[:320,5],':',label='Total Loss')
+axes[1].set_title("Content Transfer")
+axes[1].legend()
+
